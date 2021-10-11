424bc8f8a4e29298488e82e4feae7522c852b107
zohaib166 <zohaib166@gmail.com>
2021-10-11 18:46:56 +0000

    template

---
+++ OOP/template.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+template <class Z>
+Z maxim(Z x, Z y)
+{
+    if(x>y)
+        return x; 
+    else 
+        return y;
+}
+int main()
+{
+    cout << maxim(4,5) << endl;
+}      

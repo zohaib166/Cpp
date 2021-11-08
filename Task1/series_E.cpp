b4888c55d30189673d474a369accd5f61f4edc4c
zohaib166 <zohaib166@gmail.com>
2021-11-08 13:39:50 +0000

    task1

---
+++ Task1/series_E.cpp
@@ -0,0 +1,22 @@
+/* sum of the series 1x2x3, 4x5x6, 7x8x9,...n
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "Enter the term you want to print: ";
+    cin >> n;
+	int i=0,j=0;
+	int nt=0;
+	while(i<n)
+	{
+		nt = (j+1) * (j+2) * (j+3);
+		j+=3;
+		i++;
+	}
+
+	cout << nt << endl;
+	return 0;
+}

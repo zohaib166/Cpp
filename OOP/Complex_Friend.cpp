24fafaba820d4fe14f9bfa9f012ed5be474c426a
zohaib166 <92329661+zohaib166@users.noreply.github.com>
2021-11-11 01:03:54 +0530

    Add files via upload

---
+++ OOP/Complex_Friend.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+class complex {
+public:
+    double real;
+    double imag;
+    complex(double, double);
+    complex();
+    friend complex operator+(complex,complex);
+    void display();
+    friend ostream& operator<<(ostream&, complex&);
+    friend istream& operator>>(istream&, complex&);
+};
+complex::complex() {}
+complex::complex(double r, double i) {
+    real = r;
+    imag = i;
+}
+complex operator+(complex a, complex b) {
+    complex x;
+    x.real = a.real+b.real;
+    x.imag = a.imag+b.imag;
+    return x;
+}
+void complex::display() {
+    cout << real << "+i" << imag << endl;
+}
+int main() {
+    complex c1(3.4,5.6), c2(1.3, 7.3);
+    complex a1,a2,a3;
+    a3 = c1 + c2;
+    a3.display();
+    operator<<(cout,a3);
+    cout << a3;
+}

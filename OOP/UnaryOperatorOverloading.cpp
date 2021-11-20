c52546171ba2f2e0c39e6d61fd35fdc30aab0273
zohaib166 <zohaib166@gmail.com>
2021-11-20 03:42:51 +0000

    more unary

--- OOP/UnaryOperatorOverloading.cpp
+++ OOP/UnaryOperatorOverloading.cpp
@@ -5,19 +5,38 @@ public:
     double real;
     double imag;
     complex(double, double);
-    void operator-();
+    complex operator-();
+    complex operator++();
+    complex operator++(int);
     void display();
 };
 complex::complex(double r=0.0, double i=0.0) {
     real = r;
     imag = i;
 }
-void complex::operator-() {
+complex complex::operator-() {
     real = real*(-1);
     imag = imag*(-1);
+    return *this;
+}
+//pre-increment
+complex complex::operator++() {
+    real++;
+    imag++;
+    return *this;
+}
+//post-increment
+complex complex::operator++(int) {
+    real++;
+    imag++;
+    return *this;
 }
 int main() {
     complex c1(3.4,5.6);
+    cout << c1.real << "+i" << c1.imag << endl;
+    ++c1;
+    cout << c1.real << "+i" << c1.imag << endl;
+    c1++;
     cout << c1.real << "+i" << c1.imag << endl;
     -c1;
     cout << c1.real << "+i" << c1.imag << endl;

16b8fb42819d3a95eb11bac49f495c64f2585279
zohaib166 <zohaib166@gmail.com>
2021-11-25 17:50:18 +0000

    VirtualFunc

---
+++ OOP/VirtualFunction2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class Parent {
+public:
+    virtual void display() {
+        cout << "Parent class" << endl;
+    }
+};
+class Child:public Parent {
+public:    
+    void display() {
+        cout << "Child class" << endl;
+    }
+};
+int main() {
+    Parent *ptr = new Child();
+    ptr->display();
+}

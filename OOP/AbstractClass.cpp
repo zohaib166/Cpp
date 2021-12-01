673bad38b99b837c60f8cec022b57c1531369f9e
zohaib166 <92329661+zohaib166@users.noreply.github.com>
2021-12-01 23:00:43 +0530

    Add files via upload

---
+++ OOP/AbstractClass.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class Animal {
+public:
+    virtual void makeSound() = 0;
+    void eat() {
+        cout << "Munch";
+    }
+};
+class Dog:public Animal {
+public:
+    void makeSound() {
+        cout << "Bow Wow" << endl;
+    }
+};
+class Cat: public Animal {
+public:
+    void makeSound() {
+        cout << "Meow" << endl;
+    }
+};
+int main() {
+    Animal a; //not allowed
+    Animal *ptr; //Allowed
+    ptr -> eat();
+}

61e477c0d2969d18afea86268a6b3fee92fceffe
zohaib166 <zohaib166@gmail.com>
2021-12-10 13:51:44 +0000

    writing objects

--- Files/WritingObject.cpp
+++ Files/WritingObject.cpp
@@ -17,7 +17,10 @@ int main() {
     Employee e1(1,"Michael",450.50f);
     Employee e2(2,"Johannes",320.78f);
     ofstream ofile("Employee.txt");
-    if(!ofile) cerr<<"File not opened\n"; return -1;
+    if(!ofile) {
+        cerr<<"File not opened\n";
+        return -1;
+    }
     ofile<<e1.emp_id<<"\t"<<e1.name<<"\t"<<e1.salary<<"\n";
     ofile<<e2.emp_id<<"\t"<<e2.name<<"\t"<<e2.salary<<"\n";
     cout<<"Content Written to file\n";

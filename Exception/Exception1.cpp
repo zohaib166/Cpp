cbbc7682350035c01f2764e542e9aa6301ecf739
zohaib166 <zohaib166@gmail.com>
2021-12-11 18:05:28 +0000

    Exception

---
+++ Exception/Exception1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Start\n";
+    try {
+        cout << "Inside try block\n";
+        throw 100;
+        cout << "This line will not execute\n";
+    }
+    catch(int i) {
+        cout << "Caught an exception -- value is: ";
+        cout << i << "\n";
+    }
+
+    cout<<"End";
+}

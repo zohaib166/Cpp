b68bf176bb02d71d4905928dc1c9c6a5f9d092a9
zohaib166 <zohaib166@gmail.com>
2021-12-11 07:11:38 +0000

    updated Cmdline code

--- Files/CommandLine.cpp
+++ Files/CommandLine.cpp
@@ -2,17 +2,21 @@
 #include <fstream>
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
     ifstream infile;
-    infile.open("data.txt");
-    if(infile.is_open()) {
-        string str1,str2;
-        getline(infile,str1);
-        getline(infile,str2);
-        cout<<str1<<"\n"<<str2;
-        infile.close();
-    } else {
-        cout<<"File could not be opened";
+    if(argc!=2) {
+        cerr<<"Usage CommandLine <filename>"<<endl;
+        return 1;
     }
+    infile.open(argv[1]);
+    while(infile.is_open()) {
+        string str;
+        getline(infile,str);
+        cout<<str<<endl;
+        if(infile.eof()) {
+            cout<<"File ended"<<endl;
+            infile.close();
+        }
+    } 
 }

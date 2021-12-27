4011f288fbbca1b012b8d1d547ac3005e9ee095e
ZOHAIB HASAN <zohaib166@gmail.com>
2021-12-27 17:33:49 +0000

    lambda

--- Special/lambdafunction.cpp
+++ Special/lambdafunction.cpp
@@ -4,4 +4,7 @@ using namespace std;
 int main()
 {
     [](){cout<<"hello"<<endl;}();
+    [](int x, int y){cout<<"Addition is: "<<x+y<<endl;}(2,3);
+    int a = [](int x, int y)->int{return x+y;}(10,20);
+    cout << a << endl;
 }
